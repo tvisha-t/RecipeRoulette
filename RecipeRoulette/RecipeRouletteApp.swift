7995cdfe8d7ac4de85fb9a5229b70b131fc72d75
Tvisha Talwar <talwa029@umn.edu>
2024-10-22 00:32:27 -0500

    Initial Commit

---
+++ RecipeRoulette/RecipeRouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeRouletteApp.swift
+//  RecipeRoulette
+//
+//  Created by Tvisha Talwar on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeRouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
